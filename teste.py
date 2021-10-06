b21c38b09648811620282cc76142d53a31185f6c
Fabio Souza <fabiohcsouza@pm.me>
2021-10-05 23:25:37 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
@@ -10,7 +11,7 @@ class MyApp(object):
         """Constructor"""
         self.root = parent
         self.root.title("Analise rede VoIP - Net2Phone")
-        self.frame = tk.Frame(parent).grid()
+        self.frame = tk.Frame(parent).pack()
 
         # Widget Label superior
         self.lb1 = ttk.Label(self.frame, text=('TROUBLESHOOT VOIP - github.com/fabiohcsouza/Analise_rede_VoIP'),
@@ -19,84 +20,116 @@ class MyApp(object):
         anchor=tk.CENTER,
         padding=5,
         width=132,
-        ).grid()
+        )
+        
         self.root.tk.call('wm', 'iconphoto', self.root._w, tk.PhotoImage(file='images/net2phone.png'))
 
+        self.lb1.pack(fill="x") #lb superior
 
     #-Widget-Label----------------------------------------------------------
         # Widget Notebook
-        self.nb1 = ttk.Notebook(self.frame, width=796, height=500)
-        self.nb1.grid()
+        self.nb1 = ttk.Notebook(self.frame)
+        self.nb1.pack(fill="both", expand=True, padx=10, pady=10)
 
-        self.frame1 = tk.Frame(self.nb1)
-        self.frame1.grid()
+        self.frame2 = ttk.Frame(self.nb1)
+        self.frame2.pack(fill="both", expand=True)
 
-        self.frame2 = tk.Frame(self.frame1)
-        self.frame2.grid(row=0,column=0)
+        self.frame1 = ttk.LabelFrame(self.frame2, text="MTR", relief=SUNKEN)
+        self.frame1.grid(row=0, column=0, padx=5, pady=5)
 
-        self.frame3 = tk.Frame(self.frame1)
-        self.frame3.grid(row=0,column=1)
+        self.nb1.add(self.frame2, text="Troubleshoot")
 
-        self.nb1.add(self.frame1, text="Troubleshoot")
-
-        # Widget Label2
-        self.lb2 = ttk.Label(self.frame2, text=('Selecione o portal:'),
-        padding=5,
+        # Widget lb portal
+        self.lb2 = ttk.Label(self.frame1, text=('Select portal:'),
+        width=20,
         )
-        self.lb2.grid(row=0, column=0)
 
-        # Widgt seleção portal
+        # Widgt CCB portais
         self.ccb1_var = tk.Variable
-        self.ccb1 = ttk.Combobox(self.frame2, textvariable=self.ccb1_var, 
+        self.ccb1 = ttk.Combobox(self.frame1, textvariable=self.ccb1_var, 
         values=core.portais, 
-        height=10,
+        width=17,
         )
         self.ccb1.bind("<<ComboboxSelected>>", self.op_check1)
-        self.ccb1.grid(row=0, column=1)
         
-        # Widget Label3
-        self.lb3 = ttk.Label(self.frame3, text="- None",
-        padding=5,
+        # Widget lb output portal
+        self.lb3 = ttk.Label(self.frame1, text="- None",
+        width=30,
         )
-        self.lb3.grid(row=0, column=2)
 
-        # Widget Label4
-        self.lb4 = ttk.Label(self.frame2, text="Informe IP gateway:",
-        padding=5,
+        # Widget lb gateway
+        self.lb4 = ttk.Label(self.frame1, text="Enter IP gateway:",
+        width=20,
         )
-        self.lb4.grid(row=1, column=0)
 
-        # Widget Entry 1
+        # Widget input gateway
         self.entry1_var = StringVar
-        self.entry1 = ttk.Entry(self.frame2, textvariable=self.entry1_var,
+        self.entry1 = ttk.Entry(self.frame1, textvariable=self.entry1_var,
+        width=20,
         )
-        self.entry1.grid(row=1, column=1)
 
-        # Widget Label5
-        self.lb5 = ttk.Label(self.frame3, text="Informe IP Host:",
-        padding=5,
+        # Widget lb host
+        self.lb5 = ttk.Label(self.frame1, text="Enter IP Host:",
+        width=20,
         )
-        self.lb5.grid(row=1, column=2)
 
-        # Widget Entry 2
+        # Widget input host
         self.entry2_var = StringVar
-        self.entry2 = ttk.Entry(self.frame3, textvariable=self.entry2_var,
+        self.entry2 = ttk.Entry(self.frame1, textvariable=self.entry2_var,
+        width=20,
+        justify=tk.LEFT
         )
-        self.entry2.grid(row=1, column=3)
 
-        # Widget botão
-        handler = lambda: self.openFrame()
-        btn1 = ttk.Button(self.frame2, text="Start", command=handler)
-        btn1.grid(row=3, column=0)
+        # Widget bt start
+        self.handler = lambda: self.progress_bar(True)
+        self.btn1 = ttk.Button(self.frame1, text="Start", 
+        command=self.handler,
+        )
+
+        # Widget bt Parar
+        self.handler1 = lambda: self.progress_bar(False) 
+        self.btn2 = ttk.Button(self.frame1, text="Stop", 
+        command=self.handler1,
+        )
+
+        self.pb = ttk.Progressbar(self.frame1,
+        orient='horizontal',
+        mode='indeterminate',
+        length=200
+        )
+        
+
+        self.lb2.grid(row=0, column=0, pady=5, padx=5) #lb input portal
+        self.ccb1.grid(row=0, column=1, pady=5, padx=5) #CCB portais
+        self.lb3.grid(row=0, column=2, pady=5, padx=5) #lb output portal
+
+        self.lb4.grid(row=1, column=0, pady=5, padx=5) #lb gateway
+        self.entry1.grid(row=1, column=1, pady=5, padx=5) #input gateway
+        self.lb5.grid(row=2, column=0, pady=5, padx=5) #lb host
+        self.entry2.grid(row=2, column=1, pady=5, padx=5) #input host
+
+        self.btn1.grid(row=3, column=0) #bt Start
+        self.btn2.grid(row=3, column=1) #bt Stop
+        
+        
     #-Widget-Label-2-------------------------------------------------------
 
 
-    #-Seleção-portal-------------------------------------------------------
-    def select_input(self):
-        ccb1_var = tk.Variable
-        ccb_1 = ttk.Combobox(self.frame1, textvariable=ccb1_var)
-        ccb_1['values'] = core.portais
-        ccb_1.bind("<<ComboboxSelected>>", )
+    #-Barra-progresso------------------------------------------------------
+    def progress_bar(self, status):
+
+        self.pb.grid(row=3, column=2, pady=5, padx=5)
+
+        while True:
+            if status == True:
+                self.btn1['command'] = self.pb.start
+                break
+            elif status == False:
+                self.btn2['command'] = self.pb.stop
+                break
+            else:
+                break
+       
 
     #-Remover-janela-------------------------------------------------------
     def hide(self):
@@ -124,7 +157,7 @@ class MyApp(object):
     def show(self):
         """"""
         self.root.update()
-        self.root.deiconify()
+        #self.root.deiconify()
         
         #----------------------------------------------------------------------
     def op_check1(self, event):
