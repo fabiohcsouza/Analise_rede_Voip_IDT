824bfa983cd51a6408c77e9209c44ce97479da4a
Fabio Souza <fabiohcsouza@pm.me>
2021-10-08 19:35:50 -0300

    Atualizado ping e tracert

--- module/ping_portal.py
+++ module/ping_portal.py
@@ -1,61 +1,124 @@
 
 from time import sleep
-
-from icmplib import ICMPv4Socket, ICMPv6Socket, ICMPRequest
+import time
+from icmplib import ICMPv4Socket, ping
 from icmplib import ICMPLibError, ICMPError, TimeoutExceeded
-from icmplib import PID, is_ipv6_address
+import re
+import subprocess
 
+ipv4 = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
 
-def verbose_ping(address, count=30, interval=1, timeout=2, id=PID):
+dados_json = {
+            "name" : [],
+            "host" : [],
+            "ipRst": []
+            }
+
+def tracert(ip, name):
+    c=1
+    dados_json['name'] = (name)
+    dados_json['host'] = (ip)
+
+    for host in ip:
+        try: 
+            print(f'Rastreando a rota para {ip} com no máximo 30 saltos')
+
+            tracert = subprocess.Popen(
+                    "tracert /d /h 30 " + host,
+                    shell=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT)
+
+            lines = tracert.stdout.readlines()
+            res = b''.join(lines).decode("utf-8", "ignore")
+            
+            result = re.findall(ipv4, res)[1:]
+
+            print(f'{c} {result}')
+
+            for ip in result:
+
+                dados_json["ipRst"].append(ip)
+            c += 1
+
+        except subprocess.TimeoutExpired:
+            print('***')
+
+    ping_IP(True)
+
+def ping_IP(st):
     
-    print(f'PING {address}: 56 data bytes\n')
-    sock = ICMPv4Socket()
+    #VARIAVEIS
+    c = 1
+    contacts = {
+        "host1":{
+            "num": [],
+            "host": [], 
+            "loss": [], 
+            "sent": [], 
+            "recv": [], 
+            "avg": [],
+            "best": [],
+            "worst": [],
+            "last": [],
+            "jitter": [],
+            "hora": []
+            }}
+    if st == True:
+        countl=10000
+    else:
+        countl=0
+    
+    print(f'{"N"}   {"Host"}            {"Loss%"}     {"Sent"}     {"Recv"}     {"Avg"}       {"Best"}      {"Worst"}       {"Last:"}      {"Jitter"}       {"Hora":}')
 
-    for sequence in range(count):
-        # We create an ICMP request
-        request = ICMPRequest(
-            destination=address,
-            id=id,
-            sequence=sequence)
+    while st == True:
+        for ip in dados_json["ipRst"]:
+            try:
+                datetime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
 
-        try:
-            # We send the request
-            sock.send(request)
+                # Enviamos o pedido
+                request = ping(ip, count=1)
 
-            # We are awaiting receipt of an ICMP reply
-            reply = sock.receive(request, timeout)
+                # Estamos aguardando o recebimento de uma resposta do ICMP
+                #reply = sock.receive(request, timeout)
 
-            # We received a reply
-            # We display some information
-            print(f'  {reply.bytes_received} bytes from '
-                  f'{reply.source}: ', end='')
+                # Lançamos uma exceção se for uma mensagem de erro ICMP
+                #reply.raise_for_status()
 
-            # We throw an exception if it is an ICMP error message
-            reply.raise_for_status()
+                # Exibimos algumas informações
+                
 
-            # We calculate the round-trip time and we display it
-            round_trip_time = (reply.time - request.time) * 1000
+                #MANPULANDO SAIDA
+                address_s = request.address
+                min_rtt = float('{:.3f}'.format(request.min_rtt))
+                avg_rtt = float('{:.3f}'.format(request.avg_rtt))
+                max_rtt = float('{:.3f}'.format(request.max_rtt))
+                rtts_1 = request.rtts
+                #rtts = float('{:.0f}'.format(rtts_1[0]))
+                packets_sent = request.packets_sent
+                packets_received = request.packets_received
+                packet_loss = request.packet_loss
+                jitter = request.jitter
+                is_alive = request.is_alive
+                loss = packets_sent * packet_loss
+                
 
-            print(f'icmp_seq={sequence} '
-                  f'time={round(round_trip_time, 3)} ms')
+                # Exibimos algumas informações
 
-            # We wait before continuing
-            if sequence < count - 1:
-                sleep(interval)
+                print(f'{c}   {address_s}          {loss}       {packets_sent}        {packets_received}     {avg_rtt}       {min_rtt}     {max_rtt}       {rtts_1}      {jitter}       {datetime}')
+                sleep(2)
+                c += 1
+            except TimeoutExceeded:
+                # O tempo limite foi atingido
+                print('  Request timeout for icmp_seq ***')
 
-        except TimeoutExceeded:
-            # The timeout has been reached
-            print(f'  Request timeout for icmp_seq {sequence}')
+            except ICMPError as err:
+                # Uma mensagem de erro ICMP foi recebida
+                print(err)
 
-        except ICMPError as err:
-            # An ICMP error message has been received
-            print(err)
-
-        except ICMPLibError:
-            # All other errors
-            print('  An error has occurred.')
-
+            except ICMPLibError:
+                # Todos os outros erros
+                print('  An error has occurred.')
     print('\nCompleted.')
 
-
-verbose_ping('proxy2.idtbrasilhosted.com')
+tracert('8.8.8.8', 'portal 2')
--- module/teste.py
+++ module/teste.py
@@ -1,22 +1,20 @@
-
-import time
 import scapy.all as scapy
 import sys
 import  json
 from time import sleep
 
-class trace():
-    def __init__(self, dst, name):
+class mtrTrace():
+    def __init__(self, dst, name, st):
 
         # Dicionario
         self.dados_json = {
             "name" : [],
-            "host" : [], 
-            "numID" : [],
-            "ipRst": [],
-            "nPing" : [],
-            "vPing": [],
-        }
+            "host" : [],
+            "ipRst": []
+            }
+        # Mantem ping
+        self.st = st
+
         # Add variaveis entrada no dicionario
         self.dados_json['name'] = (name)
         self.dados_json['host'] = (dst)
@@ -28,14 +26,14 @@ class trace():
         # Valores, variavel
         name_id = self.dados_json["name"]
         dst_host = self.dados_json["host"]
+        c = 1
 
         # Tracert
         ans, unans = scapy.sr(scapy.IP(dst=dst_host, ttl=(1,30),id=scapy.RandShort())/scapy.TCP(flags=0x2), timeout=10)
         
         for snd,rcv in ans:
-            self.dados_json["numID"].append('{}'.format(snd.ttl))
             self.dados_json["ipRst"].append(rcv.src)
-
+            c += 1
         self.createJson(name_id)
 
     def createJson(self, id):
@@ -44,11 +42,12 @@ class trace():
 
         print(json_str)
 
-        with open('data{}.json'.format(id), 'w') as fh:
+        with open('data_{}.json'.format(id), 'w') as fh:
             fh.write(json_str)
 
-
-dst = 'proxy2.idtbrasilhosted.com'
+dst = '8.8.8.8'
 name = 'portal 2'
 
-trace(dst, name)
+mtrTrace(dst, name, True)
+
+
